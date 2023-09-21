d2db5d0c9e344c7075210881336a8c251580eb92
TaSaCoding <142542869+TaSaCoding@users.noreply.github.com>
2023-09-21 21:03:05 +0200

    forecast and styling changes

--- src/index.js
+++ src/index.js
@@ -61,6 +61,7 @@ function displayWeatherCondition(response) {
     weatherIconUrls[response.data.weather[0].icon]
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
   getForecast(response.data.coord);
 }
 function searchCity(city) {
@@ -97,37 +98,41 @@ function displayCelsiusTemperature(event) {
   fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-function formatDayForcast(timestamp) {
+function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
   return days[day];
 }
+
 function displayForecast(response) {
-  let dailyForcast = response.data.daily;
+  console.log(response.data);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row weather-forecast" >`;
-  dailyForcast.forEach(function (forecastday, index) {
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
     if (index < 5) {
       forecastHTML =
         forecastHTML +
-        `<div class="col">
-    <img
+        `
+      <div class="card grid" style="width: 5rem;">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
                 class="weather-icon"
-                src= ${weatherIconUrls[element.weather[0].icon]}
+                src= ${weatherIconUrls[forecastDay.weather[0].icon]}
                 alt=""
                 width="70"
-          />
-    <div class="weather-forecast-date">${forecastday.dt}</div>
-    <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max">${Math.round(
-        forecastday.temp.max
-      )}</span>
-      <span class="weather-forecast-temperature-min">${Math.round(
-        forecastday.temp.min
-      )}</span>
-    </div>
-  </div>
+                />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>|
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
   `;
     }
   });
@@ -136,10 +141,12 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  let apiKey = "caa883a4a60d93878755b08a933f74ea";
+  console.log(coordinates);
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+
 let searchForm = document.querySelector("#city-input");
 searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#currentLocation");
@@ -151,4 +158,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 weekTime();
 searchCity("Cologne");
-displayforecast();
+displayForecast();
